3c01a1c50dfc0d4277be82cea910975acabb046b
Kevin Ghadyani <Saturn2888@gmail.com>
2026-05-12 02:32:47 -0500

    fix: fixes e2e tests

--- e2e/playwright.setup.ts
+++ e2e/playwright.setup.ts
@@ -3,7 +3,7 @@ try {
 } catch {}
 
 export const port = Number(process.env.PORT ?? 3000)
-export const webPort = Number(process.env.WEB_PORT ?? 5173)
+export const webPort = Number(process.env.WEB_PORT ?? (process.env.CI ? 4173 : 5173))
 
 export const apiBaseUrl = `http://localhost:${port}`
 export const webBaseUrl = `http://localhost:${webPort}`
